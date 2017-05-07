66358b5fd03a330dd62367aad343b78bcc4533c1
Jguer <joaogg3@gmail.com>
2017-05-07 12:13:30 +0100

    Reordered package functions. Commented and added package Origin function

--- package.go
+++ package.go
@@ -69,6 +69,7 @@ func (l PackageList) SortBySize() PackageList {
 // DependList describes a linkedlist of dependency type packages.
 type DependList struct{ *list }
 
+// ForEach executes an action on each package of the DependList.
 func (l DependList) ForEach(f func(Depend) error) error {
 	return l.forEach(func(p unsafe.Pointer) error {
 		dep := convertDepend((*C.alpm_depend_t)(p))
@@ -76,6 +77,7 @@ func (l DependList) ForEach(f func(Depend) error) error {
 	})
 }
 
+// Slice converts the DependList to a Depend Slice.
 func (l DependList) Slice() []Depend {
 	slice := []Depend{}
 	l.ForEach(func(dep Depend) error {
@@ -85,58 +87,30 @@ func (l DependList) Slice() []Depend {
 	return slice
 }
 
-func (pkg Package) Name() string {
-	return C.GoString(C.alpm_pkg_get_name(pkg.pmpkg))
-}
-
-func (pkg Package) Version() string {
-	return C.GoString(C.alpm_pkg_get_version(pkg.pmpkg))
-}
-
+// Architecture returns the package target Architecture.
 func (pkg Package) Architecture() string {
 	return C.GoString(C.alpm_pkg_get_arch(pkg.pmpkg))
 }
 
-func (pkg Package) Description() string {
-	return C.GoString(C.alpm_pkg_get_desc(pkg.pmpkg))
+// Backup returns a list of package backups.
+func (pkg Package) Backup() BackupList {
+	ptr := unsafe.Pointer(C.alpm_pkg_get_backup(pkg.pmpkg))
+	return BackupList{(*list)(ptr)}
 }
 
-func (pkg Package) URL() string {
-	return C.GoString(C.alpm_pkg_get_url(pkg.pmpkg))
-}
-
-func (pkg Package) Packager() string {
-	return C.GoString(C.alpm_pkg_get_packager(pkg.pmpkg))
-}
-
-func (pkg Package) MD5Sum() string {
-	return C.GoString(C.alpm_pkg_get_md5sum(pkg.pmpkg))
-}
-
-func (pkg Package) SHA256Sum() string {
-	return C.GoString(C.alpm_pkg_get_sha256sum(pkg.pmpkg))
-}
-
+// BuildDate returns the BuildDate of the package.
 func (pkg Package) BuildDate() time.Time {
 	t := C.alpm_pkg_get_builddate(pkg.pmpkg)
 	return time.Unix(int64(t), 0)
 }
 
-func (pkg Package) InstallDate() time.Time {
-	t := C.alpm_pkg_get_installdate(pkg.pmpkg)
-	return time.Unix(int64(t), 0)
+// Conflicts returns the conflicts of the package as a DependList.
+func (pkg Package) Conflicts() DependList {
+	ptr := unsafe.Pointer(C.alpm_pkg_get_conflicts(pkg.pmpkg))
+	return DependList{(*list)(ptr)}
 }
 
-func (pkg Package) Size() int64 {
-	t := C.alpm_pkg_get_size(pkg.pmpkg)
-	return int64(t)
-}
-
-func (pkg Package) ISize() int64 {
-	t := C.alpm_pkg_get_isize(pkg.pmpkg)
-	return int64(t)
-}
-
+// DB returns the package's origin database.
 func (pkg Package) DB() *Db {
 	ptr := C.alpm_pkg_get_db(pkg.pmpkg)
 	if ptr == nil {
@@ -145,49 +119,105 @@ func (pkg Package) DB() *Db {
 	return &Db{ptr, pkg.handle}
 }
 
-func (pkg Package) Files() []File {
-	c_files := C.alpm_pkg_get_files(pkg.pmpkg)
-	return convertFilelist(c_files)
-}
-
+// Depends returns the package's dependency list.
 func (pkg Package) Depends() DependList {
 	ptr := unsafe.Pointer(C.alpm_pkg_get_depends(pkg.pmpkg))
 	return DependList{(*list)(ptr)}
 }
 
-func (pkg Package) Conflicts() DependList {
-	ptr := unsafe.Pointer(C.alpm_pkg_get_conflicts(pkg.pmpkg))
-	return DependList{(*list)(ptr)}
+// Description returns the package's description.
+func (pkg Package) Description() string {
+	return C.GoString(C.alpm_pkg_get_desc(pkg.pmpkg))
 }
 
-func (pkg Package) Provides() DependList {
-	ptr := unsafe.Pointer(C.alpm_pkg_get_provides(pkg.pmpkg))
-	return DependList{(*list)(ptr)}
+// Files returns the file list of the package.
+func (pkg Package) Files() []File {
+	cFiles := C.alpm_pkg_get_files(pkg.pmpkg)
+	return convertFilelist(cFiles)
 }
 
-func (pkg Package) Replaces() DependList {
-	ptr := unsafe.Pointer(C.alpm_pkg_get_replaces(pkg.pmpkg))
-	return DependList{(*list)(ptr)}
-}
-
-func (pkg Package) Backup() BackupList {
-	ptr := unsafe.Pointer(C.alpm_pkg_get_backup(pkg.pmpkg))
-	return BackupList{(*list)(ptr)}
-}
-
+// Groups returns the groups the package belongs to.
 func (pkg Package) Groups() StringList {
 	ptr := unsafe.Pointer(C.alpm_pkg_get_groups(pkg.pmpkg))
 	return StringList{(*list)(ptr)}
 }
 
+// ISize returns the package installed size.
+func (pkg Package) ISize() int64 {
+	t := C.alpm_pkg_get_isize(pkg.pmpkg)
+	return int64(t)
+}
+
+// InstallDate returns the package install date.
+func (pkg Package) InstallDate() time.Time {
+	t := C.alpm_pkg_get_installdate(pkg.pmpkg)
+	return time.Unix(int64(t), 0)
+}
+
+// Licenses returns the package license list.
 func (pkg Package) Licenses() StringList {
 	ptr := unsafe.Pointer(C.alpm_pkg_get_licenses(pkg.pmpkg))
 	return StringList{(*list)(ptr)}
 }
 
+// SHA256Sum returns package SHA256Sum.
+func (pkg Package) SHA256Sum() string {
+	return C.GoString(C.alpm_pkg_get_sha256sum(pkg.pmpkg))
+}
+
+// MD5Sum returns package MD5Sum.
+func (pkg Package) MD5Sum() string {
+	return C.GoString(C.alpm_pkg_get_md5sum(pkg.pmpkg))
+}
+
+// Name returns package name.
+func (pkg Package) Name() string {
+	return C.GoString(C.alpm_pkg_get_name(pkg.pmpkg))
+}
+
+// Packager returns package packager name.
+func (pkg Package) Packager() string {
+	return C.GoString(C.alpm_pkg_get_packager(pkg.pmpkg))
+}
+
+// Provides returns DependList of packages provides by package.
+func (pkg Package) Provides() DependList {
+	ptr := unsafe.Pointer(C.alpm_pkg_get_provides(pkg.pmpkg))
+	return DependList{(*list)(ptr)}
+}
+
+// Reason returns package install reason.
 func (pkg Package) Reason() PkgReason {
 	reason := C.alpm_pkg_get_reason(pkg.pmpkg)
 	return PkgReason(reason)
+}
+
+// Origin returns package origin.
+func (pkg Package) Origin() PkgFrom {
+	origin := C.alpm_pkg_get_origin(pkg.pmpkg)
+	return PkgFrom(origin)
+}
+
+// Replaces returns a DependList with the packages this package replaces.
+func (pkg Package) Replaces() DependList {
+	ptr := unsafe.Pointer(C.alpm_pkg_get_replaces(pkg.pmpkg))
+	return DependList{(*list)(ptr)}
+}
+
+// Size returns the packed package size.
+func (pkg Package) Size() int64 {
+	t := C.alpm_pkg_get_size(pkg.pmpkg)
+	return int64(t)
+}
+
+// URL returns the upstream URL of the package.
+func (pkg Package) URL() string {
+	return C.GoString(C.alpm_pkg_get_url(pkg.pmpkg))
+}
+
+// Version returns the package version.
+func (pkg Package) Version() string {
+	return C.GoString(C.alpm_pkg_get_version(pkg.pmpkg))
 }
 
 // ComputeRequiredBy returns the names of reverse dependencies of a package
