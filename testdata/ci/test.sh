03ea2b90fe5a96c649b99636549d6ccc7b7139f3
Jguer <me@jguer.space>
2019-10-16 20:09:52 +0100

    Run CI on testing if pacman is there. Run against eli-schwartz's compiled pacman-git once a week

--- testdata/ci/test.sh
+++ testdata/ci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t go-alpm-builder . || exit $?
+docker build -f "${1:-Dockerfile}" -t go-alpm-"${2:-builder}" . || exit $?
 
-docker run --rm go-alpm-builder make || exit $?
+docker run --rm go-alpm-"${2:-builder}" make || exit $?
 
-docker run --rm go-alpm-builder make test || exit $?
+docker run --rm go-alpm-"${2:-builder}" make test || exit $?
