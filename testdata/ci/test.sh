355f9dca7f85e870b91708aa65c9a57ac6e24489
Jguer <me@jguer.space>
2020-08-25 23:43:55 +0100

    ci(builder): use builder and remove outdated tests

--- testdata/ci/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -f "${1:-Dockerfile}" -t go-alpm-"${2:-builder}" . || exit $?
-
-docker run --rm go-alpm-"${2:-builder}" make || exit $?
-
-docker run --rm go-alpm-"${2:-builder}" make test || exit $?
