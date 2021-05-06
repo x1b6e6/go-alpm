0ad3134a2ccb223fb6bfb107143d6a54ffcf7da1
x1b6e6 <ftdabcde@gmail.com>
2021-05-06 16:18:35 +0700

    callbacks_six_test.go: Make test more strict
    Log callback must called only once at handle release.
    So this will increase `cnt.cnt` only once.

--- callbacks_six_test.go
+++ callbacks_six_test.go
@@ -3,35 +3,25 @@
 package alpm
 
 import (
-	"fmt"
 	"testing"
 )
-
-type ICnt interface {
-	get() int
-}
 
 type Cnt struct {
 	cnt int
 }
 
-func (c *Cnt) get() int {
-	ret := c.cnt
-	c.cnt += 1
-	return ret
-}
-
 func TestCallbacks(t *testing.T) {
 	h, _ := Initialize("/", "/var/lib/pacman")
-	defer h.Release()
+	cnt := &Cnt{cnt: 0}
 
 	h.SetLogCallback(func(ctx interface{}, lvl LogLevel, msg string) {
 		cnt := ctx.(*Cnt)
-		fmt.Printf("go-alpm: cnt(%d) %s\n", cnt.get(), msg)
-	}, &Cnt{cnt: 0})
+		cnt.cnt++
+	}, cnt)
 
-	h.SetQuestionCallback(func(ctx interface{}, question QuestionAny) {
-		cnt := ctx.(*Cnt)
-		fmt.Printf("question: cnt(%d)", cnt.get())
-	}, &Cnt{cnt: 2})
+	h.Release()
+
+	if cnt.cnt != 1 {
+		panic(nil)
+	}
 }
